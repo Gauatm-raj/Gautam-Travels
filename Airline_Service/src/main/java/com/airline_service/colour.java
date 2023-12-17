6ccaa83d4983c3dde51d820a541b9800660d1827
Gauatm-raj <gautamraj7569@gmail.com>
2023-12-17 20:35:38 +0530

    Booking

---
+++ Airline_Service/src/main/java/com/airline_service/colour.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.airline_service;
+
+import java.awt.Color;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class colour {
+    public static final colour x=new colour();
+    
+    public Color primary(){
+        return new Color(35,43,42);
+    }
+    public Color second(){
+        return new Color(45,62,82);
+    }
+
+}
